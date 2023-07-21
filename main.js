f783bb1f5278f68720b32b28983c85f57ee49d48
EmiGarBar <110506022+EmiGarBar@users.noreply.github.com>
2023-07-20 19:41:01 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -11,26 +11,7 @@ function setup ()
     canvas=createCanvas(480,380);
     canvas.center();
 }
-function draw () {
-image(video, 0,0,480,380);
 
-if(status != "")
-{
-    objectDetector.detect(video, gotResult);
-    for(i=0; i<objects.lenght; i++)
-    {
-        document.getElementById("status").innerHTML="objeto detectado";
-        document.getElementById("number_of_objects").innerHTML="numero de objetos detectados: "+objects.lenght;
-
-        fill("#FF0000");
-        percent=floor(objects[i].confidence*100);
-        text(objects[i].label+" "+percent+"%", objects[i].x+15, objects[i].y+15);
-        noFill();
-        stroke("#FF0000");
-        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
-    }
-}
-}
 function start () {
     objectDetector=ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML="detectando objetos"
@@ -50,3 +31,24 @@ function modelLoaded () {
     console.log(results);
     objects=results;
 }
+function draw () {
+    image(video, 0,0,480,380);
+    
+    if(status != "")
+    {
+        console.log("rectangulo");
+        objectDetector.detect(video, gotResult);
+        for(i=0; i<objects.lenght; i++)
+        {
+            document.getElementById("status").innerHTML="objeto detectado";
+            document.getElementById("number_of_objects").innerHTML="numero de objetos detectados: "+objects.lenght;
+    
+            fill("#FF0000");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%", objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+    }
